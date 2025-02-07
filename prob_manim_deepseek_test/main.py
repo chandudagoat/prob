f06329eef0697d9fe6b10b2ed073512695cd02fa
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-02-07 12:18:23 +0530

    got manim working

--- prob_manim_deepseek_test/main.py
+++ prob_manim_deepseek_test/main.py
@@ -1,5 +1,12 @@
-from dotenv import dotenv_values 
+from manim import *
+from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 deepseek_key = config["DEEPSEEK_API_KEY"]
 
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+        self.play(Create(circle))
