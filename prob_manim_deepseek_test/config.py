cacc1cb6014e5962726536797167b2368b317354
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-02-08 12:46:53 +0530

    gotta add money to both... need to think of smth here... might just run deepseek locally so gotta do some research on that

--- prob_manim_deepseek_test/config.py
+++ prob_manim_deepseek_test/config.py
@@ -2,6 +2,6 @@ from openai import OpenAI
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
-deepseek_key = config["DEEPSEEK_API_KEY"]
+openai_key = config["OPENAI_API_KEY"]
 
-deepseek_client = OpenAI(api_key=deepseek_key, base_url="https://api.deepseek.com")
+deepseek_client = OpenAI(api_key=openai_key)
--- prob_manim_deepseek_test/main.py
+++ prob_manim_deepseek_test/main.py
@@ -2,7 +2,9 @@ from manim import *
 from config import deepseek_client
 
 response = deepseek_client.chat.completions.create(
-    model="deepseek-chat", messages=[{"role": "user", "content": "Hello"}], stream=False
+    model="gpt-40",
+    messages=[{"role": "user", "content": "write a poem about charisma"}],
+    stream=False,
 )
 
-print(response.choices[0].message.content)
+print(response.choices[0].message)
